266dbbc1b9f2411fd0265733764bd279d4674a98
Janescobar92 <jancore92@gmail.com>
2020-11-19 10:50:44 +0000

    adding arry on store to implmente fake values on the card

--- src/js/component/vpetscard.jsx
+++ src/js/component/vpetscard.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { Context } from "../store/appContext.js";
+
+import { Link } from "react-router-dom";
 
 export const PetsCard = () => {
-	return (
-		<div className="card" style={{ width: "18rem" }}>
+	const { store, actions } = useContext(Context);
+	const cards = store.animals.map((animal, index) => (
+		<div className="card" style={{ width: "18rem" }} key={index}>
 			<div className="card-header">
-				Name
-				<img className="card-img-top" src="..." alt="pet" />
+				{animal.name}
+				<img className="card-img-top" src={animal.image} alt="pet" />
 				<a href="#" className="btn btn-primary">
 					Edit
 				</a>
 			</div>
 			<div className="card-body">
 				<h5 className="card-title">Info</h5>
-				<p className="card-text">Mi --- se llama ---, Tiene --- años.</p>
-				<p className="card-text">Es --- ---- pesa ---- y tiene in tamaño de ---, es ----.</p>
-				<p className="card-text">Affecciones: ---</p>
-				<p className="card-text">Sterilizado: ---</p>
+				<p className="card-text">
+					Mi {animal.animal_type} se llama {animal.name} Tiene {animal.age} años.
+				</p>
+				<p className="card-text">
+					Es un {animal.animal_type} {animal.personality} pesa {animal.weight}
+					kg y tiene un tamaño de {animal.size}
+					cm es {animal.sex ? "Hembra" : "Macho"}.
+				</p>
+				<p className="card-text">Affecciones: {animal.diseases}</p>
+				<p className="card-text">Esterilizado: {animal.sterilized ? "Si" : "No"}</p>
 			</div>
 		</div>
-	);
+	));
+	return cards;
 };
