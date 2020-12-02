3001ebdba93be17f016dc1ce3af79b3b536dc89a
Janescobar92 <jancore92@gmail.com>
2020-12-02 15:20:12 +0000

    adding css tu pets container txt and button

--- src/js/component/pets-cards-container.jsx
+++ src/js/component/pets-cards-container.jsx
@@ -4,6 +4,7 @@ import { PetsCard } from "./vpetscard.jsx";
 import { PetsForm } from "./pets_form.jsx";
 import { Context } from "../store/appContext";
 import "../../styles/containers_margins.scss";
+import "../../styles/profile_containers.scss";
 
 export const PetsCardContainer = () => {
 	const { store, actions } = useContext(Context);
@@ -11,10 +12,14 @@ export const PetsCardContainer = () => {
 	// const [showResults, setShowResults] = useState(false);
 	// const handleClick = () => setShowResults(!showResults);
 	return (
-		<div>
-			<div>
-				<h2>Pets</h2>
-				<button onClick={() => actions.showComponent()}>Añadir</button>
+		<>
+			<div className="body-container-margins">
+				<div className="container-title-size">
+					<h2>Pets</h2>
+					<button onClick={() => actions.showComponent()}>
+						<i className="fas fa-plus-circle" />
+					</button>
+				</div>
 			</div>
 			<div
 			// className="d-flex flex-column justify-content-center"
@@ -22,6 +27,6 @@ export const PetsCardContainer = () => {
 				{store.show ? <PetsForm /> : null}
 				<PetsCard />
 			</div>
-		</div>
+		</>
 	);
 };
