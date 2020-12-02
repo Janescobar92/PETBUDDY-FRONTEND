18776607d8338d04daa2b2b77a34d20f5dc37b9e
Janescobar92 <jancore92@gmail.com>
2020-12-02 14:35:56 +0000

    adding pet card style

--- src/js/component/vpetscard.jsx
+++ src/js/component/vpetscard.jsx
@@ -6,44 +6,47 @@ import { Link } from "react-router-dom";
 
 import { UpdatePetsForm } from "./pets_update_form.jsx";
 
+import "../../styles/pet_card.scss";
+
 export const PetsCard = () => {
 	const { store, actions } = useContext(Context);
 
 	const cards = store.animals.map((animal, index) => {
 		return (
-			<div className="card align-self-center my-3" style={{ width: "18rem" }} key={index}>
-				<div className="card-header">
-					{animal.name}
-					<img className="card-img-top" src={animal.image} alt="Pet IMG" />
-					<button
-						onClick={() => {
-							// alert("Seguro que quieres editar");
-							actions.setPickedIndex(animal.id);
-							actions.setShowLogin();
-						}}
-						className="btn btn-primary">
-						<i className="fas fa-edit" />
-					</button>
-					<button
-						onClick={() => {
-							alert("Are you sure you want to delete this pet");
-							actions.deletUserPet(animal.id);
-						}}>
-						<i className="fas fa-trash" />
-					</button>
+			<div className="pet--card-style" key={index}>
+				<div className="pet--card-header">
+					<h3>{animal.name}</h3>
+					<img className="" src={animal.image} alt="Pet IMG" />
+					<div className="pet--card-buttons--container">
+						<button
+							onClick={() => {
+								// alert("Seguro que quieres editar");
+								actions.setPickedIndex(animal.id);
+								actions.setShowLogin();
+							}}>
+							<i className="fas fa-edit" />
+						</button>
+						<button
+							onClick={() => {
+								alert("Are you sure you want to delete this pet");
+								actions.deletUserPet(animal.id);
+							}}>
+							<i className="fas fa-trash" />
+						</button>
+					</div>
 				</div>
-				<div className="card-body">
-					<h5 className="card-title">Info</h5>
-					<p className="card-text">
+				<div className="pet--card-body">
+					<h5 className="">Info</h5>
+					<p className="">
 						Mi {animal.animal_type} se llama {animal.name}. Tiene {animal.age} años.
 					</p>
-					<p className="card-text">
+					<p className="">
 						Es {animal.personality}, pesa {animal.weight}
 						kg, tiene un tamaño de {animal.size}
 						cm y es {animal.gender ? "hembra" : "macho"}.
 					</p>
-					<p className="card-text">Afecciones: {animal.diseases}</p>
-					<p className="card-text">Esterilizado: {animal.sterilized ? "Si" : "No"}</p>
+					<p className="">Afecciones: {animal.diseases}</p>
+					<p className="">Esterilizado: {animal.sterilized ? "Si" : "No"}</p>
 				</div>
 			</div>
 		);
