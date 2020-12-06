b5806d0615acb29aea0e0bf904cba6e1eb1c9a91
Janescobar92 <jancore92@gmail.com>
2020-12-06 14:13:35 +0000

    addign my hired history fetch and componentes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,42 +3,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			user_id: 1,
 			users: [],
-			animals: [
-				{
-					id: 1,
-					user_id: 1,
-					name: "Pluto",
-					image: "https://i.ebayimg.com/images/g/apgAAOSwd4tT5JgM/s-l300.jpg",
-					animal_type: "perro",
-					age: 3,
-					personality: "amigable",
-					gender: false,
-					weight: 20.0,
-					size: 40.0,
-					diseases: "no tiene",
-					sterilized: true
-				}
-			],
-			user_services: [
-				{
-					id: 1,
-					service_id: 1,
-					service: "Paseador",
-					date: "18 de Nov",
-					price: 20.0
-				}
-			],
-			services: [
-				{
-					id: 1,
-					service: "Paseador",
-					description: "Animals mean all to me.",
-					price: 20.0,
-					image:
-						"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-				}
-			],
-			yove_worked_history: []
+			animals: [],
+			user_services: [],
+			services: [],
+			yove_worked_history: [],
+			hired_history: []
 		},
 		actions: {
 			getWhoHireYouHistory: () => {
@@ -55,6 +24,25 @@ const getState = ({ getStore, getActions, setStore }) => {
 						console.log(responseAsJson);
 						var operationData = responseAsJson;
 						setStore({ yove_worked_history: [...getStore().yove_worked_history, operationData].flat() });
+					})
+					.catch(error => {
+						console.log("Error status: ", error);
+					});
+			},
+			getYourHireHistory: () => {
+				fetch("https://3000-c948bd0b-ac9d-4c50-a69e-4fc330593eb4.ws-eu03.gitpod.io/user/hired/1", {
+					method: "GET"
+				})
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.status);
+						}
+						return response.json();
+					})
+					.then(responseAsJson => {
+						console.log(responseAsJson);
+						var operationData = responseAsJson;
+						setStore({ hired_history: [...getStore().hired_history, operationData].flat() });
 					})
 					.catch(error => {
 						console.log("Error status: ", error);
