129356c9c3b8a7d7c396be9eeb11995392e35b5a
Janescobar92 <jancore92@gmail.com>
2020-12-06 20:45:13 +0000

    edit button

--- src/js/component/description_card.jsx
+++ src/js/component/description_card.jsx
@@ -1,26 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../../styles/description_card.scss";
 import { Context } from "../store/appContext.js";
 import { useParams } from "react-router-dom";
 
 export const DescriptionCard = () => {
+	const [dislpayForm, setdislpayForm] = useState(false);
 	const { store, actions } = useContext(Context);
 	let id_user = useParams();
 	const userToFind = store.users.find(user => user.id == id_user.id_user);
 
-	return (
+	let FilledCard = (
 		<div className="description--card-size">
+			<button onClick={() => setdislpayForm(true)}>
+				<i className="fas fa-edit" />
+			</button>
 			<div className="description--card--body-style">
 				<p>{userToFind.biografy}</p>
 			</div>
 			<div className="description--card--footer-style">
 				<p> Telf: {userToFind.phone} </p>
-				{""}
 				<p> Dirección: {userToFind.location} </p>
-				{""}
 				<p> Email: {userToFind.email} </p>
-				{""}
 			</div>
 		</div>
 	);
+
+	let FilledForm = (
+		<div className="description--card-size">
+			<button onClick={() => setdislpayForm(false)}>
+				<i className="fas fa-check-circle" />
+			</button>
+			Soy un formulario
+		</div>
+	);
+
+	if (dislpayForm == false) {
+		return FilledCard;
+	} else {
+		return FilledForm;
+	}
 };
