6a3e64687bb647f5db0e19c31ba4e5150d46d9f1
Janescobar92 <jancore92@gmail.com>
2021-01-03 16:12:20 +0000

    adding image avatars to horizontal cards

--- src/js/component/h_card.js
+++ src/js/component/h_card.js
@@ -3,12 +3,13 @@ import { Context } from "../store/appContext.js";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "../../styles/h_card_style.scss";
+import avatar from "../../assets/img/havatar.png";
 
 export const HorizontalCard = props => {
 	return (
 		<div className="card-container">
 			<div className="img-container">
-				<img src={props.source} className="" alt="Profile picture" />
+				<img src={props.source || avatar} className="" alt="Profile picture" />
 			</div>
 			<div className="text-container">
 				<h5 className="card-title text-dark">{props.name}</h5>
