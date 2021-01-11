18f0d5decacff186672089a634dfd64cdfd3f355
Janescobar92 <jancore92@gmail.com>
2021-01-11 21:19:09 +0000

    adding avatar image to history views cards

--- src/js/component/cards-container.jsx
+++ src/js/component/cards-container.jsx
@@ -28,7 +28,6 @@ export const CardContainerYouWorkedFor = () => {
 			</div>
 		);
 	}
-
 };
 
 export const CardContainerhired = () => {
--- src/js/component/hcard.jsx
+++ src/js/component/hcard.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import "../../styles/h_card_style.scss";
+import avatar from "../../assets/img/havatar.png";
 
 export const HorizontalCard = props => {
 	return (
 		<div className="card-container">
 			<div className="img-container">
-				<img src={props.source} className="" alt="Profile picture" />
+				<img src={props.source || avatar} className="" alt="Profile picture" />
 			</div>
 			<div className="text-container">
 				<h5 className="card-title">{props.name}</h5>
